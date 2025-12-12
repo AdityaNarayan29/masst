71facff4deaa209d3b7b2c68607b2ddb51ceb461
Aditya Narayan <adityanarayan29j@gmail.com>
2025-12-13 00:53:19 +0400

    feat: Masst ui landing updates

---
+++ apps/landing/tailwind.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', '../../packages/mast-ui/src/**/*.{js,ts,jsx,tsx}'],
+};
+
+export default config;
